7bf0559cac5ad596913889ac1c03abc93c12eb41
hzwgis <hzzgis@foxmail.com>
2016-10-25 20:50:31 +0800

    first created

---
+++ app/src/main/java/com/example/hzw/remindersapp/RemindersActivity.java
@@ -0,0 +1,13 @@
+package com.example.hzw.remindersapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemindersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminders);
+    }
+}
